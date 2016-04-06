49fc00e5a77218da83a4b7a1cc9bae673370186f
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-04-06 11:14:59 -0400

    Update cpuid script
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- benchmark-cpuid.sh
+++ benchmark-cpuid.sh
@@ -3,21 +3,21 @@
 CC=gcc
 CFLAGS="-pthread -Wall -O2 -g"
 
-RUNLIST="test-cpuid-thread-local-abi
-test-cpuid-thread-local-abi-lazy
-test-cpuid-baseline
-test-cpuid-glibc-test-cpuid
-test-cpuid-inline-test-cpuid
-test-cpuid-syscall
-test-cpuid-gs"
+RUNLIST="benchmark-cpuid-thread-local-abi
+benchmark-cpuid-thread-local-abi-lazy
+benchmark-cpuid-baseline
+benchmark-cpuid-glibc-getcpu
+benchmark-cpuid-inline-getcpu
+benchmark-cpuid-syscall
+benchmark-cpuid-gs"
 
-${CC} ${CFLAGS} -DCONFIG_THREAD_LOCAL_ABI -o test-cpuid-thread-local-abi test-cpuid.c
-${CC} ${CFLAGS} -DCONFIG_THREAD_LOCAL_ABI_LAZY -o test-cpuid-thread-local-abi-lazy test-cpuid.c
-${CC} ${CFLAGS} -DCONFIG_BASELINE -o test-cpuid-baseline test-cpuid.c
-${CC} ${CFLAGS} -DCONFIG_GLIBC_GETCPU -o test-cpuid-glibc-test-cpuid test-cpuid.c
-${CC} ${CFLAGS} -DCONFIG_INLINE_GETCPU -o test-cpuid-inline-test-cpuid test-cpuid.c
-${CC} ${CFLAGS} -DCONFIG_GETCPU_SYSCALL -o test-cpuid-syscall test-cpuid.c
-${CC} ${CFLAGS} -DCONFIG_GETCPU_GS -o test-cpuid-gs test-cpuid.c
+${CC} ${CFLAGS} -DCONFIG_THREAD_LOCAL_ABI -o benchmark-cpuid-thread-local-abi benchmark-cpuid.c
+${CC} ${CFLAGS} -DCONFIG_THREAD_LOCAL_ABI_LAZY -o benchmark-cpuid-thread-local-abi-lazy benchmark-cpuid.c
+${CC} ${CFLAGS} -DCONFIG_BASELINE -o benchmark-cpuid-baseline benchmark-cpuid.c
+${CC} ${CFLAGS} -DCONFIG_GLIBC_GETCPU -o benchmark-cpuid-glibc-getcpu benchmark-cpuid.c
+${CC} ${CFLAGS} -DCONFIG_INLINE_GETCPU -o benchmark-cpuid-inline-getcpu benchmark-cpuid.c
+${CC} ${CFLAGS} -DCONFIG_GETCPU_SYSCALL -o benchmark-cpuid-syscall benchmark-cpuid.c
+${CC} ${CFLAGS} -DCONFIG_GETCPU_GS -o benchmark-cpuid-gs benchmark-cpuid.c
 
 for a in ${RUNLIST}; do
 	echo "Running ./${a} 10 0"
