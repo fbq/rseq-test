3720caba4d6a862185f83f2aa2122030e925ce38
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-04-06 11:18:02 -0400

    Rename examples
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- example-tlabi-lazy.c
+++ example-tlabi-lazy.c
@@ -1,5 +1,5 @@
 /*
- * example-tlabi.c
+ * example-tlabi-lazy.c
  *
  * Copyright (c) 2015-2016 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
  *
@@ -32,10 +32,10 @@
 #include <stddef.h>
 #include "thread_local_abi.h"
 
+#define __NR_thread_local_abi       326
+
 #define likely(x)      __builtin_expect(!!(x), 1)
 #define unlikely(x)    __builtin_expect(!!(x), 0)
-
-#define __NR_thread_local_abi       326
 
 static inline int
 thread_local_abi(uint32_t tlabi_nr,
@@ -53,29 +53,39 @@ thread_local_abi(uint32_t tlabi_nr,
 __thread __attribute__((weak))
 volatile struct thread_local_abi __thread_local_abi;
 
+__thread uint32_t example_feature_fail_mask;
+
 static
 int tlabi_cpu_id_register(void)
 {
-	if (thread_local_abi(0, &__thread_local_abi, TLABI_FEATURE_CPU_ID, 0))
+	if (thread_local_abi(0, &__thread_local_abi, TLABI_FEATURE_CPU_ID, 0)) {
 		return -1;
+	}
 	return 0;
 }
 
 static
 int32_t read_cpu_id(void)
 {
-	if (unlikely(!(__thread_local_abi.features & TLABI_FEATURE_CPU_ID)))
-		return sched_getcpu();
+	if (unlikely(!(__thread_local_abi.features & TLABI_FEATURE_CPU_ID))) {
+		if (example_feature_fail_mask & TLABI_FEATURE_CPU_ID)
+			goto fallback;
+		if (tlabi_cpu_id_register()) {
+			example_feature_fail_mask |= TLABI_FEATURE_CPU_ID;
+			fprintf(stderr, "Unable to initialize thread-local ABI cpu_id feature.\n");
+			fprintf(stderr, "Using sched_getcpu() as fallback.\n");
+			goto fallback;
+		}
+	}
 	return __thread_local_abi.cpu_id;
+
+fallback:
+	return sched_getcpu();
 }
 
 int
 main(int argc, char **argv)
 {
-	if (tlabi_cpu_id_register()) {
-		fprintf(stderr, "Unable to initialize thread-local ABI cpu_id feature.\n");
-		fprintf(stderr, "Using sched_getcpu() as fallback.\n");
-	}
 	printf("Current CPU number: %d\n", read_cpu_id());
 	printf("TLABI features: 0x%x\n", __thread_local_abi.features);
 	
