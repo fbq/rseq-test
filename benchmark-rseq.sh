3d707b7b8271ed20474565ac55cd973b82f1eef9
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-07-21 15:24:59 -0400

    Add benchmark script for rseq
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ benchmark-rseq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TESTS="s i M I C"
+
+NR_THREADS=1
+
+for a in ${TESTS}; do
+	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
+	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
+done
+
+NR_THREADS=8
+
+for a in ${TESTS}; do
+	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
+	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
+done
