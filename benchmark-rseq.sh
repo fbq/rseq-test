8cf87bf3a25553973acaf743462f4a78aa61a728
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-09-26 17:25:40 -0400

    update nr loops
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- benchmark-rseq.sh
+++ benchmark-rseq.sh
@@ -7,29 +7,29 @@ NR_THREADS=1
 
 #baseline
 for a in "b"; do
-	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
-	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
+	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 1000000000
+	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 1000000000
 done
 
 for a in ${TESTS_PERCPU}; do
-	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
-	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
+	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 1000000000
+	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 1000000000
 done
 
 for a in ${TESTS_GLOBAL}; do
-	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
-	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
+	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 1000000000
+	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 1000000000
 done
 
 
 NR_THREADS=8
 
 for a in ${TESTS_PERCPU}; do
-	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
-	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 500000000
+	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 1000000000
+	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 1000000000
 done
 
 for a in ${TESTS_GLOBAL}; do
-	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 5000000
-	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 5000000
+	echo ./benchmark-rseq -T $a -t ${NR_THREADS} -r 100000000
+	time ./benchmark-rseq -T $a -t ${NR_THREADS} -r 100000000
 done
